8eb22988ca3fb3aba21247c4069addcdfca815c9
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-11-22 22:19:17 +0100

    Add --open to use xdg-open to open result

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import           Control.Monad (when)
 import           Data.Maybe (isJust)
+import           Data.Text (unpack)
 import qualified Data.Text.IO as T
 import           Piddif
 import           System.Environment (getArgs)
+import           System.IO.Temp (writeSystemTempFile)
+import           System.Process (callProcess)
 
 parseArg :: String -> Options -> Options
 parseArg arg opts = case arg of
@@ -12,6 +16,7 @@ parseArg arg opts = case arg of
   "--md"       -> opts { _mode = Markdown }
   "--markdown" -> opts { _mode = Markdown }
   "--org"      -> opts { _mode = Org }
+  "--open"     -> opts { _open = True }
   filename     -> if isJust $ _infile opts
     then opts { _outfile = Just filename }
     else opts { _infile = Just filename }
@@ -19,14 +24,22 @@ parseArg arg opts = case arg of
 data Options = Options { _help :: Bool
                        , _infile :: Maybe String
                        , _outfile :: Maybe String
-                       , _mode :: Mode }
+                       , _open :: Bool
+                       , _mode :: Mode
+                       }
 
 defaults :: Options
 defaults = Options { _infile = Nothing
                    , _outfile = Nothing
                    , _help = False
                    , _mode = Org
+                   , _open = False
                    }
+
+open :: FilePath -> IO ()
+open file = do
+  putStrLn $ "Opening file: " ++ file
+  callProcess "sh" ["-c", "xdg-open " ++ file]
 
 main :: IO ()
 main = do
@@ -36,5 +49,11 @@ main = do
     Just filename -> T.readFile filename
   res <- piddif (_mode opts) txt
   case _outfile opts of
-    Nothing -> T.putStrLn res
-    Just filename -> T.writeFile filename res
+    Nothing -> if _open opts
+      then do
+        file <- writeSystemTempFile "piddif.html" $ unpack res
+        open file
+      else T.putStrLn res
+    Just filename -> do
+      T.writeFile filename res
+      when (_open opts) $ open filename
