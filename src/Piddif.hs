0fbde4bcae7c86cea211bdf04d1a6c96d7c599b5
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-11-22 21:05:17 +0100

    Switch to local stylesheet

--- src/Piddif.hs
+++ src/Piddif.hs
@@ -19,19 +19,8 @@ renderHtml markup = return $ renderMarkup $ do
     H.head $ do
       H.title "Piddif rendered HTML"
       H.meta ! A.charset "utf-8"
-      H.link
-        ! A.href "https://fonts.googleapis.com/css?family=Roboto+Mono|Roboto:400,500"
-        ! A.rel "stylesheet"
-      H.link
-        ! A.href "https://myme.no/css/default.css"
-        ! A.rel "stylesheet"
-      H.style $ H.toHtml style
+      H.link ! A.href "./src/default.css" ! A.rel "stylesheet"
     H.body markup
-    where
-      style :: String
-      style = "h2 { font-size: 18px; }\
-              \.TODO { color: #6b6; background-color: #d9f9d9; padding: 2px 5px; }\
-              \.DONE { color: #666; background-color: #ddd; padding: 2px 5px;  }"
 
 data Mode = Markdown | Org
 
