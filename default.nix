ba787d7e66e37d47c624d4df1d4daaf3527f88ac
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-11-14 22:56:09 +0100

    nix: Use cabal2nix

--- default.nix
+++ default.nix
@@ -1,30 +1,7 @@
-{
-  pkgs ? import ./nixpkgs.nix {},
-  haskellPackages ? pkgs.haskellPackages,
-}:
+{ haskellPackages, nix-gitignore }:
 
 let
-  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  srcs = nix-gitignore.gitignoreSourcePure ./.gitignore ./.;
 
-in haskellPackages.mkDerivation {
-  pname = "piddif";
-  version = "0.1.0.0";
-  src = gitignore ./.;
-  isLibrary = true;
-  isExecutable = true;
-  executableHaskellDepends = with haskellPackages; [
-    blaze-html
-    blaze-markup
-    file-embed
-    filepath
-    optparse-applicative
-    pandoc
-    process
-    temporary
-    text
-  ];
-  testDepends = with haskellPackages; [
-    hspec
-  ];
-  license = pkgs.lib.licenses.mit;
-}
+in
+  haskellPackages.callCabal2nix "piddif" srcs {}
