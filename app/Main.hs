fd0d811d5f1b75baa6e47373de855398ba476c1c
Martin Myrseth <mm@myme.no>
2024-04-26 16:45:55 +0200

    Formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,61 +1,93 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Main where
 
-import           Control.Applicative (Alternative, (<|>), (<**>), empty, optional)
-import           Data.Maybe (fromMaybe)
-import           Data.Text (unpack)
+import Control.Applicative (Alternative, empty, optional, (<**>), (<|>))
+import Data.Maybe (fromMaybe)
+import Data.Text (unpack)
 import qualified Data.Text.IO as T
 import qualified Options.Applicative as Opts
 import qualified Piddif as P
-import           System.Exit (die)
-import           System.FilePath (takeExtension)
-import           System.IO.Temp (writeSystemTempFile)
-import           System.Process (callProcess)
+import System.Exit (die)
+import System.FilePath (takeExtension)
+import System.IO.Temp (writeSystemTempFile)
+import System.Process (callProcess)
 
-data Options = Options { _infile :: Maybe String
-                       , _outfile :: Maybe String
-                       , _open :: Bool
-                       , _mode :: Maybe P.Mode
-                       , _defaultStyles :: Bool
-                       , _inlineStyles :: Maybe String
-                       , _customStylesheet :: Maybe String
-                       , _toc :: Maybe Int
-                       }
+data Options = Options
+  { _infile :: Maybe String,
+    _outfile :: Maybe String,
+    _open :: Bool,
+    _mode :: Maybe P.Mode,
+    _defaultStyles :: Bool,
+    _inlineStyles :: Maybe String,
+    _customStylesheet :: Maybe String,
+    _toc :: Maybe Int
+  }
 
 modeParser :: Opts.Parser (Maybe P.Mode)
-modeParser = optional
-   $ Opts.flag' P.Markdown (Opts.long "md" <>
-                            Opts.long "markdown" <>
-                            Opts.help "parse input as markdown")
-  <|> Opts.flag' P.Org (Opts.long "org" <>
-                        Opts.help "parse input as org-mode")
+modeParser =
+  optional $
+    Opts.flag'
+      P.Markdown
+      ( Opts.long "md"
+          <> Opts.long "markdown"
+          <> Opts.help "parse input as markdown"
+      )
+      <|> Opts.flag'
+        P.Org
+        ( Opts.long "org"
+            <> Opts.help "parse input as org-mode"
+        )
 
 argParser :: Opts.Parser Options
-argParser = Options
-  <$> optional (Opts.strArgument (
-                   Opts.metavar "infile" <>
-                   Opts.help "defaults to stdin"))
-  <*> optional (Opts.strArgument (
-                   Opts.metavar "outfile" <>
-                   Opts.help "defaults to stdout"))
-  <*> Opts.switch (Opts.short 'o' <>
-                   Opts.long "open" <>
-                   Opts.help "open result in a browser")
-  <*> modeParser
-  <*> (not <$> Opts.switch (
-        Opts.short 'C' <>
-        Opts.long "no-css" <>
-        Opts.help "no default styles"))
-  <*> optional (Opts.strOption (
-                 Opts.long "css" <>
-                 Opts.metavar "css" <>
-                 Opts.help "inline styles"))
-  <*> optional (Opts.strOption (
-                 Opts.long "stylesheet" <>
-                 Opts.metavar "stylesheet" <>
-                 Opts.help "custom stylesheet"))
-  <*> optional (Opts.option Opts.auto (
-                   Opts.long "toc" <>
-                   Opts.help "add table of contents"))
+argParser =
+  Options
+    <$> optional
+      ( Opts.strArgument
+          ( Opts.metavar "infile"
+              <> Opts.help "defaults to stdin"
+          )
+      )
+    <*> optional
+      ( Opts.strArgument
+          ( Opts.metavar "outfile"
+              <> Opts.help "defaults to stdout"
+          )
+      )
+    <*> Opts.switch
+      ( Opts.short 'o'
+          <> Opts.long "open"
+          <> Opts.help "open result in a browser"
+      )
+    <*> modeParser
+    <*> ( not
+            <$> Opts.switch
+              ( Opts.short 'C'
+                  <> Opts.long "no-css"
+                  <> Opts.help "no default styles"
+              )
+        )
+    <*> optional
+      ( Opts.strOption
+          ( Opts.long "css"
+              <> Opts.metavar "css"
+              <> Opts.help "inline styles"
+          )
+      )
+    <*> optional
+      ( Opts.strOption
+          ( Opts.long "stylesheet"
+              <> Opts.metavar "stylesheet"
+              <> Opts.help "custom stylesheet"
+          )
+      )
+    <*> optional
+      ( Opts.option
+          Opts.auto
+          ( Opts.long "toc"
+              <> Opts.help "add table of contents"
+          )
+      )
 
 openFile :: FilePath -> IO ()
 openFile file = do
@@ -64,16 +96,18 @@ openFile file = do
 
 guessMode :: Options -> Either String P.Mode
 guessMode opts =
-  fromMaybe (Left "Cannot guess mode: provide --md or --org")
-         $  (Right <$> _mode opts)
-        <|> (fromExtension <$> _infile opts)
+  fromMaybe (Left "Cannot guess mode: provide --md or --org") $
+    (Right <$> _mode opts)
+      <|> (fromExtension <$> _infile opts)
   where
-    fromExtension fname = let ext = takeExtension fname in case ext of
-      ".md"  -> Right P.Markdown
-      ".org" -> Right P.Org
-      _      -> Left $ "Unknown extension: " <> ext
+    fromExtension fname =
+      let ext = takeExtension fname
+       in case ext of
+            ".md" -> Right P.Markdown
+            ".org" -> Right P.Org
+            _ -> Left $ "Unknown extension: " <> ext
 
-emptyIf :: Alternative f => (a -> Bool) -> a -> f a
+emptyIf :: (Alternative f) => (a -> Bool) -> a -> f a
 emptyIf f x = if f x then empty else pure x
 
 main :: IO ()
@@ -83,7 +117,7 @@ main = do
     Left err -> die err
     Right mode -> do
       txt <- maybe T.getContents T.readFile (_infile opts >>= emptyIf ("-" ==))
-      let pOpts = P.Options mode (_defaultStyles opts) (_inlineStyles opts) (_customStylesheet opts) (_toc opts)
+      let pOpts = P.Options mode opts._defaultStyles opts._inlineStyles opts._customStylesheet opts._toc
       res <- P.piddif pOpts txt
 
       fname <- case _outfile opts of
@@ -92,5 +126,5 @@ main = do
         Nothing -> T.putStrLn res >> pure Nothing
 
       case fname of
-          Just f | _open opts -> openFile f
-          _ -> pure ()
+        Just f | _open opts -> openFile f
+        _ -> pure ()
--- src/Piddif.hs
+++ src/Piddif.hs
@@ -2,45 +2,47 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Piddif
-    ( Mode(..)
-    , Options(..)
-    , normalizeHeadlines
-    , piddif
-    ) where
+  ( Mode (..),
+    Options (..),
+    normalizeHeadlines,
+    piddif,
+  )
+where
 
-import           Control.Arrow ((>>>))
-import           Control.Monad ((>=>), when)
-import           Data.FileEmbed (embedStringFile)
-import           Data.Maybe (fromMaybe, isJust)
+import Control.Arrow ((>>>))
+import Control.Monad (when, (>=>))
+import Data.FileEmbed (embedStringFile)
+import Data.Maybe (fromMaybe, isJust)
 import qualified Data.Text as T
-import           Text.Blaze ((!))
+import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import           Text.Blaze.Renderer.String (renderMarkup)
-import qualified Text.Pandoc as Pandoc
-import           Text.Pandoc
-  ( getDefaultExtensions
-  , readerExtensions
-  , pandocExtensions
-  , PandocIO
-  , readMarkdown
-  , writeHtml5
-  , runIOorExplode
-  , def
-  , readOrg
-  , writerTableOfContents
-  , writerTOCDepth
-  , writerTemplate
+import Text.Blaze.Renderer.String (renderMarkup)
+import Text.Pandoc
+  ( PandocIO,
+    def,
+    getDefaultExtensions,
+    pandocExtensions,
+    readMarkdown,
+    readOrg,
+    readerExtensions,
+    runIOorExplode,
+    writeHtml5,
+    writerTOCDepth,
+    writerTableOfContents,
+    writerTemplate,
   )
+import qualified Text.Pandoc as Pandoc
 
 data Mode = Markdown | Org
 
-data Options = Options { _mode :: Mode
-                       , _defaultstyles :: Bool
-                       , _css :: Maybe String
-                       , _stylesheet :: Maybe String
-                       , _toc :: Maybe Int
-                       }
+data Options = Options
+  { _mode :: Mode,
+    _defaultstyles :: Bool,
+    _css :: Maybe String,
+    _stylesheet :: Maybe String,
+    _toc :: Maybe Int
+  }
 
 defaultCss :: H.Html
 defaultCss = $(embedStringFile "./src/default.css")
@@ -56,40 +58,49 @@ renderHtml opts markup = return $ renderMarkup $ do
       maybe (pure mempty) (renderCss . H.toHtml) (_css opts)
       maybe (pure mempty) renderCssLink (_stylesheet opts)
     H.body markup
-  where renderCss css = H.style ! A.type_ "text/css" $ css
-        renderCssLink url = H.link ! A.rel "stylesheet" ! A.href (H.stringValue url)
+  where
+    renderCss css = H.style ! A.type_ "text/css" $ css
+    renderCssLink url = H.link ! A.rel "stylesheet" ! A.href (H.stringValue url)
 
 normalizeHeadlines :: Mode -> T.Text -> T.Text
 normalizeHeadlines mode input = T.unlines . map liftHeader $ ls
-  where ls = T.lines input
-        headerChar = case mode of
-          Markdown -> '#'
-          Org -> '*'
-        headerLevel line = case T.span (== headerChar) line of
-          (prefix, rest) | T.null prefix || not (T.isPrefixOf " " rest) -> 0
-                         | otherwise -> T.length prefix
-        isHeader = (> 0) . headerLevel
-        toplevel = minimum $ map headerLevel $ filter isHeader ls
-        liftHeader line | isHeader line = T.drop (toplevel - 1) line
-                        | otherwise = line
+  where
+    ls = T.lines input
+    headerChar = case mode of
+      Markdown -> '#'
+      Org -> '*'
+    headerLevel line = case T.span (== headerChar) line of
+      (prefix, rest)
+        | T.null prefix || not (T.isPrefixOf " " rest) -> 0
+        | otherwise -> T.length prefix
+    isHeader = (> 0) . headerLevel
+    toplevel = minimum $ map headerLevel $ filter isHeader ls
+    liftHeader line
+      | isHeader line = T.drop (toplevel - 1) line
+      | otherwise = line
 
 piddif :: Options -> T.Text -> IO T.Text
 piddif opts =
-  let
-    mode = _mode opts
-    generate Markdown = readMarkdown def { readerExtensions = getDefaultExtensions "markdown" }
-    generate Org      = readOrg def { readerExtensions = pandocExtensions }
-    normalize = normalizeHeadlines mode
-    withToc = isJust $ _toc opts
-    tocTemplate = if withToc
-      then Just $ either error id $ either (error . show) id $
-        Pandoc.runPure $ Pandoc.runWithDefaultPartials $
-        Pandoc.compileTemplate "" "<div class=\"toc\"><h1>Contents</h1>\n$toc$\n</div>\n$body$"
-      else Nothing
-    writerOptions = def { writerTableOfContents = withToc
-                        , writerTOCDepth = fromMaybe 0 $ _toc opts
-                        , writerTemplate = tocTemplate
-                        }
-    process = normalize >>> generate mode >=> writeHtml5 writerOptions >=> renderHtml opts
-  in
-    (T.pack <$>) . runIOorExplode . process
+  let mode = _mode opts
+      generate Markdown = readMarkdown def {readerExtensions = getDefaultExtensions "markdown"}
+      generate Org = readOrg def {readerExtensions = pandocExtensions}
+      normalize = normalizeHeadlines mode
+      withToc = isJust $ _toc opts
+      tocTemplate =
+        if withToc
+          then
+            Just $
+              either error id $
+                either (error . show) id $
+                  Pandoc.runPure $
+                    Pandoc.runWithDefaultPartials $
+                      Pandoc.compileTemplate "" "<div class=\"toc\"><h1>Contents</h1>\n$toc$\n</div>\n$body$"
+          else Nothing
+      writerOptions =
+        def
+          { writerTableOfContents = withToc,
+            writerTOCDepth = fromMaybe 0 $ _toc opts,
+            writerTemplate = tocTemplate
+          }
+      process = normalize >>> generate mode >=> writeHtml5 writerOptions >=> renderHtml opts
+   in (T.pack <$>) . runIOorExplode . process
