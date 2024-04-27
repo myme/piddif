3ff31a3f10f0f2b142fe831ee67e660eac95b189
Martin Myrseth <mm@myme.no>
2024-04-27 14:31:00 +0200

    server: Add basic request logging

--- server/Main.hs
+++ server/Main.hs
@@ -18,46 +18,53 @@ import qualified Options.Applicative as Opts
 import qualified Piddif as P
 import qualified System.IO as IO
 
+type Handler = Wai.Request -> IO Wai.Response
+
 runServer :: Options -> IO ()
 runServer opts = Warp.run opts.port $ \request respond -> do
   let method = Wai.requestMethod request
-  case method of
-    "GET" -> showForm opts request respond
-    "POST" -> generateDoc request respond
-    _ -> respond $ Wai.responseLBS H.status405 [] "Method Not Allowed"
+  response <- case method of
+    "GET" -> showForm opts request
+    "POST" -> generateDoc request
+    _ -> pure $ Wai.responseLBS H.status405 [] "Method Not Allowed"
+  putStrLn
+    ( show (H.statusCode $ Wai.responseStatus response)
+        <> " "
+        <> B8.unpack method
+        <> " "
+        <> B8.unpack (Wai.rawPathInfo request)
+    )
+  respond response
 
-showForm :: Options -> Wai.Application
-showForm opts _ respond =
+showForm :: Options -> Handler
+showForm opts _ = do
   if opts.embed
-    then
-      respond $ Wai.responseLBS H.status200 [(H.hContentType, "text/html")] formEmbed
-    else
-      respond $ Wai.responseFile H.status200 [] "./server/form.html" Nothing
+    then pure $ Wai.responseLBS H.status200 [(H.hContentType, "text/html")] formEmbed
+    else pure $ Wai.responseFile H.status200 [] "./server/form.html" Nothing
   where
     formEmbed = BL8.fromStrict $(embedFile "./server/form.html")
 
-generateDoc :: Wai.Application
-generateDoc request respond = do
+generateDoc :: Handler
+generateDoc request = do
   (params, _) <- Wai.parseRequestBodyEx Wai.defaultParseRequestBodyOptions Wai.lbsBackEnd request
   let mode = maybe (Right P.Markdown) (P.modeParser . B8.unpack) $ lookup "mode" params
   let scheme = maybe (Right P.Light) (P.schemeParser . B8.unpack) $ lookup "scheme" params
-  case (,) <$> mode <*> scheme of
-    Left err -> respond $ Wai.responseLBS H.status400 [] (BL8.pack err)
-    Right (mode', scheme') -> case lookup "input" params of
-      Nothing -> respond $ Wai.responseLBS H.status400 [] "Bad Request: missing input"
-      Just input -> do
-        let opts =
-              P.Options
-                { P._mode = mode',
-                  P._defaultstyles = True,
-                  P._css = Nothing,
-                  P._stylesheet = Nothing,
-                  P._toc = Nothing,
-                  P._scheme = Just scheme'
-                }
-        result <- P.piddif opts (T.decodeUtf8 input)
-        let responseBody = BS.fromStrict $ T.encodeUtf8 result
-        respond $ Wai.responseLBS H.status200 [(H.hContentType, "text/html")] responseBody
+  let input = maybe (Left "Bad Request: missing input") Right $ lookup "input" params
+  case (,,) <$> mode <*> scheme <*> input of
+    Left err -> pure $ Wai.responseLBS H.status400 [] (BL8.pack err)
+    Right (mode', scheme', input') -> do
+      let opts =
+            P.Options
+              { P._mode = mode',
+                P._defaultstyles = True,
+                P._css = Nothing,
+                P._stylesheet = Nothing,
+                P._toc = Nothing,
+                P._scheme = Just scheme'
+              }
+      result <- P.piddif opts (T.decodeUtf8 input')
+      let responseBody = BS.fromStrict $ T.encodeUtf8 result
+      pure $ Wai.responseLBS H.status200 [(H.hContentType, "text/html")] responseBody
 
 data Options = Options
   { host :: String,
